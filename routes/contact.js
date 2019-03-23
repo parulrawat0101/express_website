84235220294c19921a8c08f1608c12fc938d57c7
Parul <parulrawat0101@gmail.com>
2019-03-23 22:33:39 +0530

    Activated about page, contact page

---
+++ routes/contact.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' }); //displays jade file
+});
+
+module.exports = router;
